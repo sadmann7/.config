58a1b16722af281b83e578383a9c0090b9c11cf1
sadmann7 <sadmann898@gmail.com>
2022-11-17 18:23:28 +0600

    :zap: themes > omp

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,23 @@
+# oh-my-posh
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\custom-two.omp.json" | Invoke-Expression
+
+# PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+Set-PSReadLineOption -PredictionViewStyle ListView
+
+# Fzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PsReadlineChordReverseHistory 'Ctrl+r'
+
+# Icons
+Import-Module -Name Terminal-Icons
+
+# Utilities
+function which ($command) {
+    Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
+
